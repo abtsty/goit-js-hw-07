f162669d4e66c8d0db62ea4e8d5867df39297d98
abtsty <andrey.barylov@gmail.com>
2023-10-05 14:53:50 +0300

    final

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector('.gallery');
+const item = markup(galleryItems);
+
+list.insertAdjacentHTML('afterbegin', item);
+
+function markup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+<img
+class="gallery__image"
+src="${preview}"
+data-source="${original}"
+alt="${description}"
+
+/>
+</a>
+</li>`,
+    )
+    .join('');
+}
+list.addEventListener('click', handlerClick);
+function handlerClick(evt) {
+  evt.preventDefault();
+  if (evt.target === evt.currentTarget) {
+    return;
+  }
+  const img = evt.target.closest('.gallery__image');
+
+  const makeGallery = basicLightbox.create(`
+<div class="modal">
+<img src="${img.dataset.source}" width="800" height="600">
+</div>
+`);
+
+  makeGallery.show();
+  const card = document.querySelector('.basicLightbox');
+  card.addEventListener('click', close);
+  function close(e) {
+    if (e.target !== e.currentTarget) {
+      return;
+    }
+    makeGallery.close();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const list = document.querySelector('.gallery');
+const item = markup(galleryItems);
+
+function markup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallary__item"><a href="${original}" class="gallery__link">
+    <img src="${preview}" 
+        alt="${description}" 
+        class="gallery__image"></a></li>`,
+    )
+    .join('');
+}
+
+list.insertAdjacentHTML('afterbegin', item);
+
+const newGallery = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
